ab0f39a1e6854ac5f14ff1e4ef00843150a00c55
John L <john.m.lenehan@gmail.com>
2015-11-17 18:31:56 -0500

    Finished excercies,starting model testing

--- test/itsalive.js
+++ test/itsalive.js
@@ -1,1 +1,51 @@
+var chai = require('chai');
+var expect = chai.expect;
+var spies = require('chai-spies');
+chai.use(spies);
+
 console.log("Funny");
+
+describe("simple math",function(){
+	it("should add up",function(){
+		expect(2+2).to.equal(4);
+	});
+});
+
+
+describe("A timeout function",function(){
+	it("should timeout for a given amount of time",function(done){
+		var newDate = new Date();
+		setTimeout(function(){
+			var date2 = new Date();
+			var duration = date2-newDate;
+			expect(duration).to.be.closeTo(1000,50);
+			done()
+		},1000);
+	})
+});
+
+
+describe("forEach spy",function(){
+
+	// before(function(){
+	
+	// });	
+
+
+	it("is only called 5 times",function(){
+		var array = [1,2,3,4,5];
+		var funcObj = {
+			func: function(a){console.log(a)}
+		}
+		var spy = chai.spy.on(funcObj,"func");
+		array.forEach(funcObj.func);
+
+		expect(spy).to.have.been.called.exactly(5);
+
+	})
+
+
+})
+
+
+
